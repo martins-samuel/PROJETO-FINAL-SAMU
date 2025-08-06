a4c6cc1479f52697c2958af534a64641e7231ddc
ead <ead@academico.local>
2025-08-06 17:02:36 -0300

    criado o app martkplace

--- crud_base/views.py
+++ crud_base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
+def criar_usuario(request):
     return HttpResponse ("<h1>Bem vindo a página Crud</h1>")
