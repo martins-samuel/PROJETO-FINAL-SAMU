273d53fd845070eeae50d61b189c718bf2c8effa
ead <ead@academico.local>
2025-08-06 17:27:48 -0300

    rota da pagina usuario configurado

--- crud_base/views.py
+++ crud_base/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def criar_usuario(request):
-    return HttpResponse ("<h1>Bem vindo a página Crud</h1>")
+    #return HttpResponse ("<h1>Bem vindo a página Crud</h1>")
+    return render('crud_base/usuario_lead.html')
