1b2fd5beaa4d0a10540dcba2f0aa4fbe874ebe89
ead <ead@academico.local>
2025-08-11 14:36:46 -0300

    marketplace index.html

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    #return HttpResponse ("<h1>Bem vindo a página Crud</h1>")
+    #requeste requisição
+    #templatehtml entre outros
+    #context objetos (python, python com banco de dados)
+    return render(request,'marketplace/index.html')
--- setup/urls.py
+++ setup/urls.py
@@ -20,4 +20,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('crud_base.urls')),
+    path('',include('marketplace.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
