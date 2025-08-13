fdc51815dbce3bc4411fc73bdc5e81cd6446fd87
ead <ead@academico.local>
2025-08-13 17:08:56 -0300

    13/05

--- setup/settings.py
+++ setup/settings.py
@@ -122,6 +122,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Onde o Djano vai procurar os arquivos estasticos 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'setup/static')
+]
+#Onde os arquivos estáticos serão coletados para produção
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
